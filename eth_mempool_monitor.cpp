cc5d7f8178d47119ccdb7716755b0d21b7dd850e
Adrian Edward Stan <Edward.Stan.04@gmail.com>
2024-12-03 21:52:51 -0500

    Timed parallel & serial mempool monitoring

--- eth_mempool_monitor.cpp
+++ eth_mempool_monitor.cpp
@@ -11,23 +11,21 @@
 
 #include <vector>
 
-#include <ctype.h> // used to make FROM filtered address all lowercase
-#include <stdio.h> // used to make FROM filtered address all lowercase
+#include<chrono>
 
 typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
 
-std::string ethereum_node_url; // Will be initialized in main()
-std::string from_address_filter = ""; // Will be initialized in main()
+const std::string UNISWAP_MEMPOOL_ADDRESS = "0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad";
 
 // Helper function to make an HTTP POST request to get transaction details by hash
-std::string get_transaction_details(const std::string& tx_hash) {
+std::string get_transaction_details(const std::string& tx_hash, std::string &endpoint_url) {
     CURL* curl = curl_easy_init();
     std::string result;
 
     if(curl) {
         std::string json_request = R"({"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":[")" + tx_hash + R"("],"id":1})";
 
-        curl_easy_setopt(curl, CURLOPT_URL, ethereum_node_url.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, endpoint_url.c_str());
         curl_easy_setopt(curl, CURLOPT_POST, 1L);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_request.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, +[](void* data, size_t size, size_t nmemb, std::string* writerData) -> size_t {
@@ -44,8 +42,8 @@ std::string get_transaction_details(const std::string& tx_hash) {
     return result;
 }
 
-// Function to parse and print transaction details from JSON response
-void print_transaction_details(const std::string& json_response) {
+// Function to parse transaction details from JSON response to determine if it comes from UNISWAP address
+bool is_transaction_to_uniswap(const std::string& json_response, int &transactions_printed) {
     Json::Value root;
     Json::CharReaderBuilder reader;
     std::string errs;
@@ -55,33 +53,13 @@ void print_transaction_details(const std::string& json_response) {
         auto result = root["result"];
         if (!result.isNull()) {
             std::stringstream ss; // used to convert string hex to integer
-            
-            std::string from = result["from"].asString();
+            std::string to = result["to"].asString();
             //std::cout << result["from"].asString() << std::endl;
             //std::cout << from_address_filter << std::endl << std::endl;
-            if (from == from_address_filter)
+            if (to == UNISWAP_MEMPOOL_ADDRESS)
             {
-                std::string to = result["to"].asString();
-
-                ss << std::hex << result["gas"].asString();
-                long long gas;
-                ss >> gas;
-                ss.clear();
-
-                ss << std::hex << result["gasPrice"].asString();
-                long long gasPrice;
-                ss >> gasPrice;
-                ss.clear();
-
-                ss << std::hex << result["value"].asString();
-                long long value;
-                ss >> value;
-
-                std::cout << "--------------------------" << std::endl;
-                std::cout << "From: " << from << "\nTo: " << to 
-                << "\nValue: " << result["value"].asString() << " (" << value  << ") "<< "\nGas: " << result["gas"].asString() 
-                << " (" << gas << ") "
-                << "\nGas Price: " << result["gasPrice"].asString() << " (" << gasPrice << ") " << "\n" << std::endl;
+                transactions_printed++;
+                return true;
             }
         } else {
             //std::cerr << "Transaction details not found." << std::endl; // good for debugging purposes
@@ -89,10 +67,11 @@ void print_transaction_details(const std::string& json_response) {
     } else {
         //std::cerr << "Failed to parse JSON: " << errs << std::endl;
     }
+    return false;
 }
 
 // Function to initialize the WebSocket connection and handle reconnection
-void init() {
+void parallel_init(int count, std::vector<std::string> &transaction_hashes, int &transactions_printed, std::string &endpoint_url) {
 
     boost::asio::thread_pool thread_pool(15); // thread pool of size 15!
 
@@ -114,43 +93,52 @@ void init() {
         std::cout << "Subscribed to pending transactions" << std::endl;
     });
 
-    ws_client.set_message_handler([&ws_client, &thread_pool](websocketpp::connection_hdl, client::message_ptr msg) {
+    ws_client.set_message_handler([&ws_client, &thread_pool, &count, &transaction_hashes, &transactions_printed, &endpoint_url](websocketpp::connection_hdl, client::message_ptr msg) {
+        if (transactions_printed < count)
+        {
+            boost::asio::post(thread_pool, [msg, &count, &transaction_hashes, &transactions_printed, &endpoint_url]() {
+                Json::Value root;
+                Json::CharReaderBuilder reader;
+                std::string errs;
+                std::istringstream s(msg->get_payload());
 
-        boost::asio::post(thread_pool, [msg]() {
-        Json::Value root;
-        Json::CharReaderBuilder reader;
-        std::string errs;
-        std::istringstream s(msg->get_payload());
+                if (Json::parseFromStream(reader, s, &root, &errs)) {
+                    std::string tx_hash = root["params"]["result"].asString();
+                    //std::cout << "Pending Transaction Hash: " << tx_hash << std::endl; //leaving here 4 debugging later
 
-        if (Json::parseFromStream(reader, s, &root, &errs)) {
-            std::string tx_hash = root["params"]["result"].asString();
-            //std::cout << "Pending Transaction Hash: " << tx_hash << std::endl; //leaving here 4 debugging later
+                    // Retrieve and print transaction details
+                    std::string tx_details = get_transaction_details(tx_hash, endpoint_url);
+                    if (is_transaction_to_uniswap(tx_details, transactions_printed) & (transaction_hashes.size() <= count) )
+                    {
+                        transaction_hashes.push_back(tx_hash);
+                    }
 
-            // Retrieve and print transaction details
-            std::string tx_details = get_transaction_details(tx_hash);
-            print_transaction_details(tx_details);
-        } else {
-            //std::cerr << "Failed to parse pending transaction JSON: " << errs << std::endl;
+                } else {
+                    //std::cerr << "Failed to parse pending transaction JSON: " << errs << std::endl;
+                }
+            });
         }
-        });
+        else {
+            ws_client.stop();
+        }
 
     });
 
     // Error and close handlers for reconnection
-    ws_client.set_fail_handler([&ws_client](websocketpp::connection_hdl) {
+    ws_client.set_fail_handler([&ws_client, &count, &transaction_hashes, &transactions_printed, &endpoint_url](websocketpp::connection_hdl) {
         std::cerr << "Unable to connect, retrying in 3 seconds..." << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(3));
-        init();
+        parallel_init(count, transaction_hashes, transactions_printed, endpoint_url);
     });
 
-    ws_client.set_close_handler([&ws_client](websocketpp::connection_hdl) {
+    ws_client.set_close_handler([&ws_client, &count, &transaction_hashes, &transactions_printed, &endpoint_url](websocketpp::connection_hdl) {
         std::cerr << "Connection closed! Attempting to reconnect in 3 seconds..." << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(3));
-        init();
+        parallel_init(count, transaction_hashes, transactions_printed, endpoint_url);
     });
 
     websocketpp::lib::error_code ec;
-    client::connection_ptr con = ws_client.get_connection(ethereum_node_url, ec);
+    client::connection_ptr con = ws_client.get_connection(endpoint_url, ec);
 
     if (ec) {
         std::cerr << "Could not create connection because: " << ec.message() << std::endl;
@@ -161,23 +149,136 @@ void init() {
     ws_client.run();
 }
 
+
+void serial_init(int count, std::vector<std::string> &transaction_hashes, int &transactions_printed, std::string &endpoint_url) {
+    client ws_client;
+    ws_client.init_asio();
+
+    ws_client.set_tls_init_handler([](websocketpp::connection_hdl) {
+        auto ctx = std::make_shared<websocketpp::lib::asio::ssl::context>(websocketpp::lib::asio::ssl::context::tlsv12);
+        ctx->set_options(websocketpp::lib::asio::ssl::context::default_workarounds |
+                         websocketpp::lib::asio::ssl::context::no_sslv2 |
+                         websocketpp::lib::asio::ssl::context::no_sslv3 |
+                         websocketpp::lib::asio::ssl::context::single_dh_use);
+        return ctx;
+    });
+
+    ws_client.set_open_handler([&ws_client](websocketpp::connection_hdl hdl) {
+        std::string subscribe_message = R"({"jsonrpc":"2.0","id":1,"method":"eth_subscribe","params":["newPendingTransactions"]})";
+        ws_client.send(hdl, subscribe_message, websocketpp::frame::opcode::text);
+        std::cout << "Subscribed to pending transactions" << std::endl;
+    });
+
+    ws_client.set_message_handler([&ws_client, &count, &transaction_hashes, &transactions_printed, &endpoint_url](websocketpp::connection_hdl, client::message_ptr msg) {
+        if (transactions_printed < count)
+        {
+            Json::Value root;
+            Json::CharReaderBuilder reader;
+            std::string errs;
+            std::istringstream s(msg->get_payload());
+
+            if (Json::parseFromStream(reader, s, &root, &errs)) {
+                std::string tx_hash = root["params"]["result"].asString();
+                //std::cout << "Pending Transaction Hash: " << tx_hash << std::endl; //leaving here 4 debugging later
+
+                // Retrieve and print transaction details
+                std::string tx_details = get_transaction_details(tx_hash, endpoint_url);
+                if (is_transaction_to_uniswap(tx_details, transactions_printed) & (transaction_hashes.size() <= count) )
+                {
+                    transaction_hashes.push_back(tx_hash);
+                }
+
+            } else {
+                //std::cerr << "Failed to parse pending transaction JSON: " << errs << std::endl;
+            }
+        }
+        else {
+            ws_client.stop();
+        }
+
+    });
+
+    // Error and close handlers for reconnection
+    ws_client.set_fail_handler([&ws_client, &count, &transaction_hashes, &transactions_printed, &endpoint_url](websocketpp::connection_hdl) {
+        std::cerr << "Unable to connect, retrying in 3 seconds..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        serial_init(count, transaction_hashes, transactions_printed, endpoint_url);
+    });
+
+    ws_client.set_close_handler([&ws_client, &count, &transaction_hashes, &transactions_printed, &endpoint_url](websocketpp::connection_hdl) {
+        std::cerr << "Connection closed! Attempting to reconnect in 3 seconds..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        serial_init(count, transaction_hashes, transactions_printed, endpoint_url);
+    });
+
+    websocketpp::lib::error_code ec;
+    client::connection_ptr con = ws_client.get_connection(endpoint_url, ec);
+
+    if (ec) {
+        std::cerr << "Could not create connection because: " << ec.message() << std::endl;
+        return;
+    }
+
+    ws_client.connect(con);
+    ws_client.run();
+}
+
+long long serial_mempool_monitor(int count, std::string endpoint_url) // return how long it took to collect
+{
+    std::vector<std::string> transaction_hashes;
+    int hashes_found = 0;
+    auto t0 = std::chrono::system_clock::now();
+    serial_init(count, transaction_hashes, hashes_found, endpoint_url);
+    auto t1 = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
+    //return transaction_hashes;
+}
+
+
+long long parallel_mempool_monitor(int count, std::string endpoint_url)
+{
+    std::vector<std::string> transaction_hashes;
+    int hashes_found = 0;
+    auto t0 = std::chrono::system_clock::now();
+    parallel_init(count, transaction_hashes, hashes_found, endpoint_url);
+    auto t1 = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
+    //return transaction_hashes;
+}
+
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <Ethereum Node URL>" << " <To Address>" << std::endl;
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <Ethereum Node URL 1>" << " <Ethereum Node URL 2>" 
+        << " <Transaction Count>" << std::endl;
         return 1;
     }
     
-    ethereum_node_url = argv[1]; // Set the URL from the command line argument
-    from_address_filter = argv[2];
+    int transaction_count = std::stoi(argv[3]); 
 
-    // convert address filter to a lowercase string
-    std::string temp = "";
-    for (char c : from_address_filter)
-        temp += (char) tolower(c);
-    from_address_filter = temp;
+    //std::string endpoint_url_1 = "https://wandering-chaotic-meadow.quiknode.pro/db70ddd07bd5f00cc9b937ac7015fe65c4e135e1";
+    //std::string endpoint_url_2 = "https://quick-blissful-road.quiknode.pro/233e464433e3f16b4b5200a4ba45595903d0b737";
 
+    std::string endpoint_url_1 = argv[1];
+    std::string endpoint_url_2 = argv[2];
+    
+    std::promise<long long> parallel_promise, serial_promise;
+    std::future<long long> parallel_future = parallel_promise.get_future();
+    std::future<long long> serial_future = serial_promise.get_future();
 
-    init();
+    std::thread parallel_thread([&parallel_promise] (int transaction_count, std::string endpoint_url) {
+        parallel_promise.set_value(parallel_mempool_monitor(transaction_count, endpoint_url));
+    }, transaction_count, endpoint_url_1);
+        
+    std::thread serial_thread([&serial_promise] (int transaction_count, std::string endpoint_url) {
+        serial_promise.set_value(serial_mempool_monitor(transaction_count, endpoint_url));
+    }, transaction_count, endpoint_url_2);
+
+    parallel_thread.join();
+    serial_thread.join();
+
+    std::cout << "PARALLEL TIME: " << parallel_future.get() << std::endl;
+    std::cout << "SERIAL   TIME: " << serial_future.get() << std::endl;
+
     return 0;
 }
 
